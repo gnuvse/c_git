a71815379c403d15ad01fe3b6fbf5cb43b1b3936
gnuvse <yiontsev@gmail.com>
2015-05-21 13:55:43 +0300

    Пофиксил: проверка на входе, смена типов

--- fact.c
+++ fact.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    int n, fact;
+    unsigned long long fact;
+    unsigned int n;
 
-    printf("Please, enter number: \n");
-    scanf("%i", &n);
+    printf("Please, enter number(1-20): \n");
+    scanf("%ulli", &n);
 
     fact = 1;
-    if (n >= 1) {
-        for (int i = 1; i <= n; i++) {
+    if (n >= 1 && n <= 20) {
+        for (unsigned int i = 1; i <= n; i++) {
             fact *= i;
         }
-        printf("%i! = %i\n", n, fact);
+        printf("%i! = %lli\n", n, fact);
     }
     else {
         printf("Error, n = 0");
