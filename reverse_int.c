4f1a6312eadf36f9d6474edaf4ea0d28ce70fd92
Sergei Shilovsky <sshilovsky@gmail.com>
2015-05-22 16:35:40 +0300

    Как сделать reverse от отрицательного числа

--- reverse_int.c
+++ reverse_int.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void reverse(int n);
+void reverse_int(int n);
 
 int main() {
 	int n;
@@ -13,7 +13,8 @@ int main() {
 	return 0;
 }
 
-void reverse(int n) {
+int reverse_nonnegative(int n) {
+	// Сделай так, чтобы функция возвращала результат, а не выводила его на экран
 	int right_digit;
 	do {
 		right_digit = n % 10;
@@ -21,4 +22,16 @@ void reverse(int n) {
 		n /= 10;
 	} while (n != 0);
 	printf("\n");
+}
+
+int reverse_int(int n) {
+	// TODO
+	// Не знаю, чего там хочет автор, но по хорошему, делать надо через модуль числа:
+	// У тебя уже есть функция reverse_nonegative, и чтобы не дублировать код проще
+	// воспользоваться ей.
+	// Типа, если n<0, то reverse_nonnegative(-n)
+	
+	// Если хочешь поизвращаться, сделай функцию reverse_negative отдельно.
+	// Но это не очень хорошая идея, т.к. в С % и / от отрицательного числа,
+	// если я не путаю, могут иметь разное действие, в зависимости от платформы.
 }
