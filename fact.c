6e71327b2e4e22c5b9c8e175bafff7395ce647fd
gnuvse <yiontsev@gmail.com>
2015-05-22 21:50:59 +0300

    bug fix

--- fact.c
+++ fact.c
@@ -5,18 +5,19 @@ int main() {
     unsigned int n;
 
     printf("Please, enter number(1-20): \n");
-    scanf("%ulli", &n);
+    scanf("%ui", &n);
 
-    fact = 1;
-    if (n >= 1 && n <= 20) {
-        for (unsigned int i = 1; i <= n; i++) {
-            fact *= i;
-        }
-        printf("%i! = %lli\n", n, fact);
-    }
-    else {
-        printf("Error, n = 0");
-    }
+
+    if (n < 1 && n > 20) {
+		printf("Bad interval for n");
+		return 1;
+	}
+
+	fact = 1;
+	for (unsigned int i = 1; i <= n; i++)
+		fact *= i;
+
+	printf("%i! = %lli\n", n, fact);
 
     return 0;
 }
