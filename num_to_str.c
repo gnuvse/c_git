dfed9503f1d51fd767d922dbf777c1dd6089b951
Yura Iontsev <yiontsev@gmail.com>
2015-05-23 22:27:05 +0400

    Упростил программу: избавился от не нужного реверса чисел, от лишних операций и переменных

--- num_to_str.c
+++ num_to_str.c
@@ -14,19 +14,19 @@
 
 #include <stdio.h>
 
-int reverse_nonnegative_int(int n, int count);
-int number_digits(int n, int count);
-void print_num(int reverse_n);
+void print_num(int n, int count);
+int number_digits(int n);
+void print_digit(int n);
 void num_to_str(int n, int count);
 
 
 
 int main() {
-	int n, count = 0;
+	int n;
 	printf("Please, enter number: \n");
 	scanf("%i", &n);
 
-	num_to_str(n, number_digits(n, count));
+	num_to_str(n, number_digits(n));
 
 	return 0;
 }
@@ -34,15 +34,18 @@ int main() {
 
 
 void num_to_str(int n, int count) {
-	if (count > 8)
-		printf("Error! Output abroad\n");
+	if (count > 8 || n <= 0) {
+		printf("Error! Incorrect data:\n");
+		printf("overflow or n <= 0\n");
+	}
 	else
-		print_num(reverse_nonnegative_int(n, count) );
+		print_num(n, number_digits(n));
 }
 
 
 // Находим количество цифр в числе.
-int number_digits(int n, int count) {
+int number_digits(int n) {
+	int count = 0;
 	while (n != 0) {
 		count++;
 		n /= 10;
@@ -52,80 +55,60 @@ int number_digits(int n, int count) {
 }
 
 
-// Делаем реверс числа пользователя
-int reverse_nonnegative_int(int n, int count) {
-	int right_digit;
-	int reverse_n = 0;
+
+void print_num(int n, int count) {
 	int tmp_count = count;
 	int multi;
 
 
 	for (int i = 0; i < count; i++) {
 		multi = 1;
-		right_digit = n % 10;
 
-		// 10 в степени tmp_count
-		// j > 1 сокращаем такты цикла на 1
-		// можно записать tmp_count - 1
 		for (int j = tmp_count; j > 1; j--) {
 			multi *= 10;
 		}
-
-		reverse_n += right_digit * multi;
-
-		// Уменьшаем степень
 		tmp_count--;
 
-		// Получаем следующую цифру в числе убирая  сотни, десятки и т.д
-		// Пример: 953 / 10 == 95
-		n /= 10;
+		print_digit((n / multi) % 10);
 	}
-
-	return reverse_n;
 }
 
 
-// Теперь мы снова меняем делаем реверс и уже выводим наши цифры словами
-void print_num(int reverse_n) {
-	int right_digit;
-	do {
-		right_digit = reverse_n % 10;
-		switch (right_digit) {
-			case 0:
-				printf("zero ");
-				break;
-			case 1:
-				printf("one ");
-				break;
-			case 2:
-				printf("two ");
-				break;
-			case 3:
-				printf("three ");
-				break;
-			case 4:
-				printf("four ");
-				break;
-			case 5:
-				printf("five ");
-				break;
-			case 6:
-				printf("six ");
-				break;
-			case 7:
-				printf("seven ");
-				break;
-			case 8:
-				printf("eight ");
-				break;
-			case 9:
-				printf("nine ");
-				break;
-			default:
-				printf("Error!\n");
-				break;
-        }
-		reverse_n /= 10;
-	} while (reverse_n != 0);
-	printf("\n");
+
+void print_digit(int n) {
+	switch (n) {
+		case 0:
+			printf("zero ");
+			break;
+		case 1:
+			printf("one ");
+			break;
+		case 2:
+			printf("two ");
+			break;
+		case 3:
+			printf("three ");
+			break;
+		case 4:
+			printf("four ");
+			break;
+		case 5:
+			printf("five ");
+			break;
+		case 6:
+			printf("six ");
+			break;
+		case 7:
+			printf("seven ");
+			break;
+		case 8:
+			printf("eight ");
+			break;
+		case 9:
+			printf("nine ");
+			break;
+		default:
+			printf("Error!\n");
+			break;
+    }
 }
