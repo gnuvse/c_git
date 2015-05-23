41ce0b3b73b9ce3a47f233b10188a7e7c16b741f
Yura Iontsev <yiontsev@gmail.com>
2015-05-23 15:57:17 +0400

    fix: измениль switch на for

--- num_to_str.c
+++ num_to_str.c
@@ -21,8 +21,6 @@ void num_to_str(int n, int count);
 
 
 
-
-
 int main() {
 	int n, count = 0;
 	printf("Please, enter number: \n");
@@ -34,6 +32,7 @@ int main() {
 }
 
 
+
 void num_to_str(int n, int count) {
 	if (count > 8)
 		printf("Error! Output abroad\n");
@@ -42,63 +41,47 @@ void num_to_str(int n, int count) {
 }
 
 
-// В этой функции мы находим количество цифр в числе.
+// Находим количество цифр в числе.
 int number_digits(int n, int count) {
 	while (n != 0) {
 		count++;
 		n /= 10;
 	}
+
 	return count;
 }
 
 
-
+// Делаем реверс числа пользователя
 int reverse_nonnegative_int(int n, int count) {
 	int right_digit;
 	int reverse_n = 0;
-	do {
-		right_digit = n % 10;
-        // В данном выборе, мы получаем десятки, сотни, тысячи.
-        // Если вводим 953, то получим в right_digit == 3.
-        // count == 2, соответственно получаем 300 и тд, до 359
-        // т.е. делаем реверс числа
-        // PS. count == 2, потому что отчет с 0
-		switch (count) {
-			case 1:
-				reverse_n = reverse_n + right_digit * 10;
-				break;
-			case 2:
-				reverse_n = reverse_n + right_digit * 100;
-				break;
-			case 3:
-				reverse_n = reverse_n + right_digit * 1000;
-				break;
-			case 4:
-				reverse_n = reverse_n + right_digit * 10000;
-				break;
-			case 5:
-				reverse_n = reverse_n + right_digit * 100000;
-				break;
-			case 6:
-				reverse_n = reverse_n + right_digit * 1000000;
-				break;
-			case 7:
-				reverse_n = reverse_n + right_digit * 10000000;
-				break;
-			case 8:
-				reverse_n = reverse_n + right_digit * 100000000;
-				break;
-			default:
-				printf("Error!\n");
-				break;
-        }
-        count--;
-		n /= 10;
-	} while (n != 0);
+	int tmp_count = count;
+	int multi;
 
-	// не знаю в чем дело, но к реверсивному числу добавляется 0
-	// таким не хитрым способом избавляемся от 0
-	return reverse_n / 10;
+
+	for (int i = 0; i < count; i++) {
+		multi = 1;
+		right_digit = n % 10;
+
+		// 10 в степени tmp_count
+		// j > 1 сокращаем такты цикла на 1
+		// можно записать tmp_count - 1
+		for (int j = tmp_count; j > 1; j--) {
+			multi *= 10;
+		}
+
+		reverse_n += right_digit * multi;
+
+		// Уменьшаем степень
+		tmp_count--;
+
+		// Получаем следующую цифру в числе убирая  сотни, десятки и т.д
+		// Пример: 953 / 10 == 95
+		n /= 10;
+	}
+
+	return reverse_n;
 }
 
 
