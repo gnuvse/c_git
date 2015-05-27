94fa6291b32624fe7e12c619ab4775535da76d71
Yura Iontsev <yiontsev@gmail.com>
2015-05-27 11:14:57 +0300

    добавлена проверка на входные данные

--- square_root.c
+++ square_root.c
@@ -22,6 +22,11 @@ float square_root(float x) {
 	const float epsilon = 0.00001;
 	float guess = 1.0;
 
+	if (x < 0) {
+		printf("Negative argument to square_root.\n");
+		return -1;
+	}
+
 	while(abs_val(guess * guess - x) >= epsilon) {
 		printf("guess 1 = %f\n", guess);
 		guess = (x / guess + guess) / 2.0;
