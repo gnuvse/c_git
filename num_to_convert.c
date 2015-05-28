f1f485bbc46644e565e9db533ab693bfc559b11d
Yura Iontsev <yiontsev@gmail.com>
2015-05-28 11:33:10 +0300

    вынесен код конвертации числа

--- num_to_convert.c
+++ num_to_convert.c
@@ -1,13 +1,15 @@
 // Преобразования целого числа из одной системы счисления в другую
 #include <stdio.h>
 
-int convert_number[64];
+int converted_number[64];
 long int num_to_convert;
 int next_digit, base, index = 0;
 
 
 void get_num();
 void get_base();
+void convert_number();
+
 
 int main() {
 	const char base_digits[36] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -17,23 +19,28 @@ int main() {
 	get_num();
 	get_base();
 
-    	// Преобразовать в указанное основание
-	do {
-		convert_number[index] = num_to_convert % base;
-		index++;
-		num_to_convert /= base;
-	} while (num_to_convert != 0);
+	// Преобразовать в указанное основание
+	convert_number();
 
 	// Отобразить результат в обратном порядке
 	// от n-1 до 0
 	for (index -= 1; index >= 0; index--) {
-		next_digit = convert_number[index];
+		next_digit = converted_number[index];
 		printf("%c", base_digits[next_digit]);
 	}
 
 	printf("\n");
 	return 0;
 }
+
+void convert_number() {
+	do {
+		converted_number[index] = num_to_convert % base;
+		index++;
+		num_to_convert /= base;
+	} while (num_to_convert != 0);
+}
+
 
 void get_base() {
 	printf("Base(2 .. 36)? ");
@@ -44,6 +51,8 @@ void get_base() {
 		base = 10;
 	}
 }
+
+
 
 void get_num() {
 	printf("Number to be converted? ");
