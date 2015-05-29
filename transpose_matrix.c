59b9fdf912a77c22ea31d53971e34291d0f2f471
Yura Iontsev <yiontsev@gmail.com>
2015-05-29 19:51:13 +0300

    транспонирование любого размера

--- transpose_matrix.c
+++ transpose_matrix.c
@@ -1,36 +1,58 @@
 #include <stdio.h>
 
-void transpose_matrix(int m[4][5], int n[5][4]);
+void transpose_matrix(int row, int column,
+						int m[row][column], int n[column][row]);
+
+void arr_init(int row, int column, int m[row][column]);
+
+void print_matrix(int row, int column, int matrix[row][column]);
 
 int main() {
-	int m[4][5] = {
-		{1, 2, 3, 4, 5},
-		{1, 2, 3, 4, 5},
-		{1, 2, 3, 4, 5},
-		{1, 2, 3, 4, 5}
-	};
+	int row, column;
+	printf("Enter row and column: \n");
+	scanf("%i %i", &row, &column);
 
-	int n[5][4] = {
-		{0, 0, 0, 0},
-		{0, 0, 0, 0},
-		{0, 0, 0, 0},
-		{0, 0, 0, 0},
-		{0, 0, 0, 0}
-	};
+	int m[row][column];
+	int n[column][row];
 
-	transpose_matrix(m, n);
+	arr_init(row, column, m);
+	print_matrix(row, column, m);
+	transpose_matrix(row, column, m, n);
+
 
 	return 0;
 }
 
-void transpose_matrix(int m[4][5], int n[5][4]) {
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 5; j++)
+
+void print_matrix(int row, int column, int matrix[row][column]) {
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < column; j++)
+			printf("%i ", matrix[i][j]);
+		printf("\n");
+	}
+}
+
+void arr_init(int row, int column, int m[row][column]) {
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < column; j++) {
+			printf("row#%i, column#%i == ", i, j);
+			scanf("%i", &m[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+
+void transpose_matrix(int row, int column,
+					 int m[row][column], int n[column][row]) {
+
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < column; j++)
 			n[j][i] = m[i][j];
 	}
 
-	for (int i = 0; i < 5; i++) {
-		for (int j = 0; j < 4; j++)
+	for (int i = 0; i < column; i++) {
+		for (int j = 0; j < row; j++)
 			printf("%i ", n[i][j]);
 		printf("\n");
 	}
