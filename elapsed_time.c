5cec3f44a12002dfede8191066d74afdb27e0a3a
Yura Iontsev <yiontsev@gmail.com>
2015-06-01 12:14:11 +0300

    программа выводит разницу времени

---
+++ elapsed_time.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+struct time {
+	int hour;
+	int minutes;
+	int seconds;
+};
+
+struct time elapsed_time(struct time t1, struct time t2);
+
+int main() {
+	struct time t1 = {12, 10, 10};
+	struct time t2 = {24, 10, 10};
+	struct time t_res = elapsed_time(t1, t2);
+
+	printf("t_res = %i/%i/%i\n", t_res.hour, t_res.minutes, t_res.seconds);
+
+	return 0;
+}
+
+
+struct time elapsed_time(struct time t1, struct time t2) {
+	struct time t_res;
+
+	if (t1.hour > t2.hour)
+		t_res.hour = t1.hour - t2.hour;
+	else
+		t_res.hour = t2.hour - t1.hour;
+
+	if (t1.minutes > t2.minutes)
+		t_res.minutes = t1.minutes - t2.minutes;
+	else
+		t_res.minutes = t2.minutes - t1.minutes;
+
+	if (t1.seconds > t2.seconds)
+		t_res.seconds = t1.seconds - t2.seconds;
+	else
+		t_res.seconds = t2.seconds - t1.seconds;
+
+	return t_res;
+}
