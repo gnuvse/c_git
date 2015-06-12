e92b78d507b428bbfd042bb42f080903618a3c42
Yura Iontsev <yiontsev@gmail.com>
2015-06-12 23:43:29 +0300

    Добавил сломанные тесты diff_number__const_arg

--- bignum/test.c
+++ bignum/test.c
@@ -22,6 +22,42 @@ int test_diff_number__equal_length(void)
 	return MUST_BE_TRUE(ok);
 }
 
+int test_diff_number__const_arg1(void)
+{
+	char a[] = {7, 5, 4};
+	int n = 3;
+
+	char b[] = {2, 9};
+	int m = 2;
+
+	char *res = diff_number(n, m, a, b);
+	free(res);
+
+	const char check[] = {7, 5, 4};
+
+	int ok = eq_number(n, a, 3, check);
+
+	return MUST_BE_TRUE(ok);
+}
+
+int test_diff_number__const_arg2(void)
+{
+	char a[] = {7, 5, 4};
+	int n = 3;
+
+	char b[] = {2, 9};
+	int m = 2;
+
+	char *res = diff_number(n, m, a, b);
+	free(res);
+
+	const char check[] = {2, 9};
+
+	int ok = eq_number(m, b, 2, check);
+
+	return MUST_BE_TRUE(ok);
+}
+
 
 
 int main()
@@ -29,6 +65,8 @@ int main()
 	INIT_TEST();
 
 	RUN_TEST(test_diff_number__equal_length);
+	RUN_TEST(test_diff_number__const_arg1);
+	RUN_TEST(test_diff_number__const_arg2);
 
 	FINISH_TEST();
 
