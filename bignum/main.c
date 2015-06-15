bcf70575fbe6f9393c8d88aedcdcfd5fee3c66c3
Yura Iontsev <yiontsev@gmail.com>
2015-06-15 14:10:44 +0300

    смена передачи параметра, с адреса на указатель

--- bignum/main.c
+++ bignum/main.c
@@ -11,7 +11,7 @@ int main() {
 
 	int count1;
 	int count2;
-	int num_of_elem_without_zeros;
+
 
 	count1 = input_number(SIZE, buf1);
 	printf("count = %i\n", count1);
@@ -26,10 +26,12 @@ int main() {
 	char *buf2_reversed = reverse_number(count2, buf2);
 	char *res_diff = diff_number(count1, buf1_reversed, count2, buf2_reversed);
 	char *res = reverse_number(count1, res_diff);
-	char *normalize_res = normalize_number(count1, res, &num_of_elem_without_zeros);
+
+	int *num_of_elem_without_zeros = malloc(sizeof(int));
+	char *normalize_res = normalize_number(count1, res, num_of_elem_without_zeros);
 
 
-	output_number(num_of_elem_without_zeros, normalize_res);
+	output_number(*num_of_elem_without_zeros, normalize_res);
 
 
 	free(buf1);
@@ -39,6 +41,7 @@ int main() {
 	free(res);
 	free(res_diff);
 	free(normalize_res);
+	free(num_of_elem_without_zeros);
 
 	return 0;
 }
